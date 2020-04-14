04ac56f25a6679b7148271f1e2b13978bf570838
Cynthia Rich <crichID@github.com>
2020-04-14 15:46:52 -0400

    use history instead of link to improve form submission behavior

--- client/src/pages/Join/index.js
+++ client/src/pages/Join/index.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import API from '../../utils/API';
 import { useGameContext } from '../../utils/GlobalState';
 import { ADD_PLAYER } from '../../utils/actions';
@@ -12,6 +12,7 @@ const Join = () => {
     const iconRef = useRef();
     const colorRef = useRef();
     const [state, dispatch] = useGameContext();
+    let history = useHistory();
 
     const handleSubmit = event => {
         //event.preventDefault();
@@ -27,18 +28,19 @@ const Join = () => {
                 color: colorRef.current.value
             }
         });
+        history.push('/game');
     }
 
     return (
         <div>
             <h1>Join a game!</h1>
-            <input placeholder="Enter game code" type="text" ref={gameRef} />
-            <input placeholder="Display Name" type="text" ref={nameRef} />
-            <input placeholder="Select Icon" type="text" ref={iconRef} />
-            <input placeholder="Select Color" type="text" ref={colorRef} />
-            <Link onClick={(event) => handleSubmit(event)} to='/game'>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <input placeholder="Enter game code" type="text" ref={gameRef} />
+                <input placeholder="Display Name" type="text" ref={nameRef} />
+                <input placeholder="Select Icon" type="text" ref={iconRef} />
+                <input placeholder="Select Color" type="text" ref={colorRef} />
                 <button type="submit">Join Now</button>
-            </Link>
+            </form>
         </div>
     )
 }
