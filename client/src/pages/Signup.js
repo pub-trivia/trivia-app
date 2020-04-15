adcb00996314e5d0e8784f4c3889a1f79c6d0136
Cynthia Rich <15653252+cynthiarich@users.noreply.github.com>
2020-04-14 22:08:53 -0400

    Another tiny formatting thing

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import SignupForm from '../components/SignupForm';
 import Button from '../components/Button';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import "../style.css";
 
 const Signup = () => [
     <div>
         <SignupForm />
         <Link to="/">
-        <Button type="button" text="Skip Sign Up" />
+            <Button type="button" text="Skip Sign Up" />
         </Link>
         <Link to="/login">
-        <Button type="button" text="LOG IN" />
+            <Button type="button" text="LOG IN" />
         </Link>
     </div>
 ]
