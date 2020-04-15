fc1a0fa233bb044f2498b04c23ac56f8a926d1cc
Cynthia Rich <crichID@github.com>
2020-04-15 00:50:58 -0400

    actually use the JAWSDB

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
