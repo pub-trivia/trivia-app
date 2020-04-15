ef158684ee9200c600dd8aad4133fb784ccea669
Cat Kretzschmar <cat.ktz@gmail.com>
2020-04-14 21:57:16 -0400

    links added to buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import JoinForm from "../components/JoinForm";
 import Button from '../components/Button';
+import {Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
+          <Link to="/login">
           <Button type="button" text="LOG IN" path="/login" />
+          </Link>
+          <Link to="/signup"> 
           <Button type="button" text="SIGN UP" path="/signup" />
+          </Link>
           <h2>OR Play Now...</h2>
           <JoinForm />
         </div>
