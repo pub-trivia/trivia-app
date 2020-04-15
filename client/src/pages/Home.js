54529fe1f9f6d0d7e087fd33c7beac071ed8ca64
Cynthia Rich <15653252+cynthiarich@users.noreply.github.com>
2020-04-14 22:07:58 -0400

    tiny formatting nits

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import JoinForm from "../components/JoinForm";
 import Button from '../components/Button';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <div>
           <Link to="/login">
-          <Button type="button" text="LOG IN" path="/login" />
+            <Button type="button" text="LOG IN" path="/login" />
           </Link>
           <Link to="/signup"> 
-          <Button type="button" text="SIGN UP" path="/signup" />
+            <Button type="button" text="SIGN UP" path="/signup" />
           </Link>
           <h2>OR Play Now...</h2>
           <JoinForm />
