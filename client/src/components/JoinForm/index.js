7259d9ad5d6aa9c0c6855590729fefc0b1a6fe54
Cynthia Rich <crichID@github.com>
2020-04-14 20:55:06 -0400

    move JoinForm to components and merge with PlayNowForm

--- client/src/components/JoinForm/index.js
+++ client/src/components/JoinForm/index.js
@@ -1,12 +1,13 @@
 import React, { useRef } from 'react';
 import { useHistory } from 'react-router-dom';
-import API from '../../utils/API';
 import { useGameContext } from '../../utils/GlobalState';
 import { ADD_PLAYER } from '../../utils/actions';
 
-import './Join.css';
+import Button from '../Button';
 
-const Join = () => {
+import './JoinForm.css';
+
+const JoinForm = () => {
     const gameRef = useRef();
     const nameRef = useRef();
     const iconRef = useRef();
@@ -33,16 +34,26 @@ const Join = () => {
 
     return (
         <div>
-            <h1>Join a game!</h1>
             <form onSubmit={(event) => handleSubmit(event)}>
-                <input placeholder="Enter game code" type="text" ref={gameRef} />
-                <input placeholder="Display Name" type="text" ref={nameRef} />
-                <input placeholder="Select Icon" type="text" ref={iconRef} />
-                <input placeholder="Select Color" type="text" ref={colorRef} />
-                <button type="submit">Join Now</button>
+                <label>Enter Game Code:
+                    <input placeholder="Enter game code" type="text" ref={gameRef} />
+                </label>
+                <label>Name:
+                    <input placeholder="Display Name" type="text" ref={nameRef} />
+                </label>
+                <h4>Select a profile icon and color:</h4>
+                <label>Icon:
+                    <input placeholder="Select Icon" type="text" ref={iconRef} />
+                </label>
+                <label>Color:
+                    <input placeholder="Select Color" type="text" ref={colorRef} />
+                    {/* color values: [#03E4AC, #04D5FB, #F7903E, #FFD72F, #9665D8, #F94141, #4481D8, #F96E99, #FFFFFF, #9FA1A0] */}
+                </label>
+                <Button type="submit" text="Join Now" />
+                {/* <button type="submit">Join Now</button> */}
             </form>
         </div>
     )
 }
 
-export default Join;
+export default JoinForm;
