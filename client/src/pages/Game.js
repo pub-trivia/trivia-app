6ae007343fa1b0bff46ff1e25bdb2261d3e59c53
Cynthia Rich <crichID@github.com>
2020-04-16 00:22:55 -0400

    separate waiting page from game

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,45 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import io from 'socket.io-client';
-
-import WaitingRoom from '../components/WaitingRoom';
-import { useGameContext } from '../utils/GlobalState';
-
-let socket;
+import React from 'react';
 
 const Game = () => {
-    const [state, dispatch] = useGameContext();
-    const [users, setUsers] = useState('');
-    let ENDPOINT = "http://localhost:3000"
-
-    if(process.env.NODE_ENV === "production"){
-        ENDPOINT = "https://pub-trivia.herokuapp.com"
-    }
-    
-    const { game, name, icon, color } = state;
-
-    useEffect(() => {
-        socket = io(ENDPOINT);
-        console.log(socket, game, name, icon, color);
-        socket.emit('join', { game, name, icon, color }, () => {});
-
-        return () => {
-            socket.emit('disconnect');
-            socket.off();
-        }
-    }, [ENDPOINT]);
-
-    useEffect(() => {
-        socket.on("gameData", ({ users }) => {
-            setUsers(users);
-        });
-    }, []);
-
     return (
-        <div>
-            <h1>You're in game: {state.game}</h1>
-            <WaitingRoom users={users} />
-        </div>
-        
+        <h1>Time to play the game!</h1>
     )
 }
 
