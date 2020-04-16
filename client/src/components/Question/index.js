8311db76a4c46d1f875b89b9b6c8ba3b57a33d20
Cynthia Rich <crichID@github.com>
2020-04-16 03:12:18 -0400

    add components for game play

---
+++ client/src/components/Question/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './question.css';
+
+const Question = () => {
+    return (
+        <h1>Question goes here!</h1>
+    )
+}
+
+export default Question;
---
+++ client/src/components/Scoreboard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './scoreboard.css';
+
+const Scoreboard = () => {
+    return (
+        <h3>Scoreboard goes here!</h3>
+    )
+}
+
+export default Scoreboard;
