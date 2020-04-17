8c9a587dd32df2600f5155eced9ec2e079a103c8
Cynthia Rich <crichID@github.com>
2020-04-17 02:46:07 -0400

    utils updates for authentication

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,15 +6,13 @@ export default {
     },
 
     signUpUser: (name, email, pw, icon, color) => {
-        console.log("==========sign up user==========")
-        console.log(name, email, pw, icon, color)
-        return axios.post("/api/signup", {displayName:name, email:email, password:pw, avatar:icon, avatarColor:color })
+
+        return axios.post("/api/signup", {displayName:name, email, password:pw, icon, color })
     },
 
-    loginUser: (email, pw) => {
-        console.log("==========log in user==========")
-        console.log(email, pw)
-        return axios.get("/api/login",{email:email, password:pw})
+    loginUser: (email, password) => {
+        
+        return axios.post("/api/login", {email, password})
     },
 
     getQuizbyCode: (game) => {
