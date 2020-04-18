8e09fa0baea90715b1314e29de104aef1d352543
Cynthia Rich <crichID@github.com>
2020-04-18 16:39:52 -0400

    fix error introduced in merge conflict

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,8 +6,4 @@ router.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 module.exports = router;
