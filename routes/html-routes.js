d4d2897679f0afd4f03914b68c30556d1dff4ea2
Cynthia Rich <crichID@github.com>
2020-04-18 17:02:22 -0400

    lets try this

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = router;
