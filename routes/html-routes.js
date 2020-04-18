16578d0b7d6885f9080aba82c54f47e1b46758fe
Cynthia Rich <crichID@github.com>
2020-04-18 14:13:49 -0400

    remove old / route in favor of wildcard route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Server is up and running!');
+router.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 module.exports = router;
