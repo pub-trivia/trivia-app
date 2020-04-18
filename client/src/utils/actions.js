7886d70307dce64d6241f6755a108e8928484af9
sjmars <sjmarsnc@gmail.com>
2020-04-18 12:54:38 -0400

    get all actions in actions.js

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -2,9 +2,9 @@ export const SET_GAME = "SET_GAME";
 export const SET_NAME = "SET_NAME";
 export const SET_ICON = "SET_ICON";
 export const SET_AUTH = "SET_AUTH";
+export const SET_COLOR = "SET_COLOR";
 
 export const ADD_GAME = "ADD_GAME";
-export const ADD_PLAYER = "ADD_PLAYER";
 export const ADD_PLAYER = "ADD_PLAYER";
 export const ADD_USER = "ADD_USER";
 export const LOGOUT_USER = "LOGOUT_USER";
