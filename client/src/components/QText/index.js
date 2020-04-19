bc8ee61c22fb476d49213cfa026b3f64bc2d3cd8
Cynthia Rich <crichID@github.com>
2020-04-19 16:02:59 -0400

    rename question to qtext

---
+++ client/src/components/QText/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './qtext.css';
+
+const QText = () => {
+    return (
+        <div className="qbox">
+            <span className="qtext">Question goes here!</span>
+        </div>
+    )
+}
+
+export default QText;
