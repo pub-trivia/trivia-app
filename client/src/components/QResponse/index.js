04b68896ef1afb270fb33073efdd6bb2959aebe2
Cynthia Rich <crichID@github.com>
2020-04-19 16:04:03 -0400

    rename responses to qresponses to be consistent with qtext

--- client/src/components/QResponse/index.js
+++ client/src/components/QResponse/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './responses.css';
+import './qresponse.css';
 
-const Responses = () => {
+const QResponse = () => {
     return (
         <h2>Responses go here!</h2>
         //when all users have selected an option, show correct response
     )
 }
 
-export default Responses;
+export default QResponse;
