867c12911da905488628ea3d867e0b524161afb5
Cynthia Rich <crichID@github.com>
2020-04-24 12:17:54 -0400

    cleaning up timer code

--- client/src/components/Timer/index.js
+++ client/src/components/Timer/index.js
@@ -6,16 +6,12 @@ import './timer.css';
 
 const Timer = () => {
     const [timer, setTimer] = useState();
-    //const [timeR, setTimeR] = useState();
-    //const { timeLeft } = props;
 
     useEffect(() => {
         ws.on('updateTimer', ({ text }) => {
-            console.log("=====updateTimer======");
-            console.log(text);
             setTimer(text);
         })
-    });
+    }, []);
 
     return (
         <div className="timertext">{timer > 9 ? `00:${timer}` : `00:0${timer > -1 ? timer : "0"}`}</div>
