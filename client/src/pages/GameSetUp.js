ead55a869d2e8722aff665a82dab3ab424f31d7c
Cynthia Rich <15653252+cynthiarich@users.noreply.github.com>
2020-04-24 12:04:48 -0400

    remove unnecessary component

--- client/src/pages/GameSetUp.js
+++ client/src/pages/GameSetUp.js
@@ -1,15 +1,10 @@
 import React from "react";
 import SetUpComponent from "../components/SetUpComponent";
-// import 
-// 
-// 
-// import "../style.css";
 
 function GameSetUpPage() {
   return (
     <div>
       <SetUpComponent />
-      <PubImage-maybe />
     </div>
   );
 }
