565f926423937ac4630d9b4987061c36762c694d
Cynthia Rich <crichID@github.com>
2020-06-15 23:40:14 -0400

    clean up some unused actions in global state

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,12 +1,8 @@
-export const SET_GAME = "SET_GAME";
 export const SET_ICON = "SET_ICON";
 export const SET_COLOR = "SET_COLOR";
-export const SET_AUTH = "SET_AUTH";
 export const SET_USERS = "SET_USERS";
 export const SET_SCORES = "SET_SCORES";
 
 export const ADD_GAME = "ADD_GAME";
 export const ADD_PLAYER = "ADD_PLAYER";
 export const ADD_USER = "ADD_USER";
-export const LOGOUT_USER = "LOGOUT_USER";
-export const UPDATE_USER = "UPDATE_USER";
