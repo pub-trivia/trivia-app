1ff79854e257817fd5b4db4a1f9cbceaae63aa98
sjmars <sjmarsnc@gmail.com>
2020-06-16 15:29:33 -0400

    put quizScore field back to Boolean

--- models/quizScore.js
+++ models/quizScore.js
@@ -21,7 +21,7 @@ module.exports = function (sequelize, DataTypes) {
             type: DataTypes.STRING
         },
         correct: {
-            type: DataTypes.STRING,
+            type: DataTypes.BOOLEAN,
             required: true
         }
     });
